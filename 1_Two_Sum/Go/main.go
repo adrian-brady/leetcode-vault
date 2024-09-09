6a71157a5f6d8f4fbc4d11e401b4f44df4c50765
adrian-brady <mail@adrianbrady.com>
2024-09-09 17:41:09 -0500

    add problem 1 solution

---
+++ 1_Two_Sum/Go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// complement is target - current
+// make map to hold complementary values
+// check if map holds complement
+// if not, hasnt been seen, add index to map
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if v, ok := m[v]; ok {
+			return []int{m[v], i}
+		}
+
+		complement := target - nums[i]
+		m[complement] = i
+	}
+	return []int{0, 0}
+}
+
+func main() {
+	test := []int{2, 7, 11, 15}
+	ans := twoSum(test, 9)
+	fmt.Printf("%+v\n", ans)
+}
